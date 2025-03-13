72522c0534b50fb18630e731329bd0bf4cb1f415
Lamia BOUKHERROUB <lamia.boukherroub@Brandtdz.Pri>
2025-03-13 09:52:17 +0100

    Initial commit

---
+++ add_user.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Application Utilisateurs</title>
+</head>
+<body>
+    <div class="container">
+        <div class="wrapper p-5 m-5 ">
+            <div class="d-flex p-2 justify-content-between ">
+                <h2>Add User</h2>
+                <div><a href="index.php"><i data-feather="corner-down-left"></i></a></div>
+            </div>
+            <form action="index.php" method="post"></form>
+        </div>
+    </div>
+
+
+
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/icon.js"></script>
+    <script>
+  feather.replace();
+</script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Application Utilisateurs</title>
+</head>
+<body>
+    <div class="container">
+        <div class="wrapper p-5 m-5 ">
+            <div class="d-flex p-2 justify-content-between ">
+                <h2>All Users</h2>
+                <div><a href="add_user.php"><i data-feather="user-plus"></i></a></div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/icon.js"></script>
+    <script>
+  feather.replace();
+</script>
+</body>
+</html>
