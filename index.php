83e88bdd1e181408614d1a5648d6957e27345cab
Lamia BOUKHERROUB <lamia.boukherroub@Brandtdz.Pri>
2025-04-17 09:42:44 +0100

    affichage des données sur la page all users

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 include_once('db.php');
+
+session_start(); // Démarrer la session
+
+
 if(isset($_POST['save'])){
    
     //vérifier ue les champs sont vides
@@ -14,21 +18,24 @@ if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['mobile'])
    if(!$res_add){
     die(mysqli_error($con));
    }else{
-    echo 'user added successfully';
+     
+    $_SESSION['action'] = "add";
 
     // réinitialiser les variables pour eviter quelle concervent les anciennes val
     $name = $email = $mobile = $password = "";
-
     //rafraichit la page apres linsertion pour garantir un reset complet
     header("Location: ".$_SERVER['PHP_SELF']);
             exit();
    }
 }
 }
+
+    $users_sql="SELECT * FROM user";
+    $all_user=mysqli_query($con,$users_sql);
+    $user=$all_user->fetch_assoc();
+    
+
 ?>
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -42,10 +49,40 @@ if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['mobile'])
 <body>
     <div class="container">
         <div class="wrapper p-5 m-5 ">
-            <div class="d-flex p-2 justify-content-between ">
+            <div class="d-flex p-2 justify-content-between mb-2">
                 <h2>All Users</h2>
                 <div><a href="add_user.php"><i data-feather="user-plus"></i></a></div>
             </div>
+            <hr>  <!-- inserer une ligne horizontale -->
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Mobile</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                      while ($user = $all_user->fetch_assoc()) { ?>
+                        <tr>
+                            <td><?php echo $user['id']; ?></td>
+                            <td><?php echo $user['name']; ?></td>
+                            <td><?php echo $user['email']; ?></td>
+                            <td><?php echo $user['mobile']; ?></td>
+                            <td>
+                                <div class="d-flex p-2 justify-content-evenly mb-2">
+                                    <i onclick="confirm_delete(f" class="text-danger" data-feather="trash-2"></i>
+                                    <i class="text-success" data-feather="edit"></i>
+                                </div>
+                                
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+
         </div>
     </div>
 
@@ -54,9 +91,18 @@ if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['mobile'])
     <script src="js/bootstrap.min.js"></script>
     <script src="js/icon.js"></script>
     <script src="js/toastr.js"></script>
+    <script src="js/main.js"></script>
+    <?php
+    if (isset($_SESSION['action'])){
+        if($_SESSION['action'] == "add" ){?>
+        <script>
+            show_add()
+        </script>
+    <?php
+         unset($_SESSION['action']);}  // fermer la session de msG de validaton de cretion
+        }?>
     <script>
-  feather.replace();
-  toastr.info('Are you the 6 fingered man?')
-</script>
+        feather.replace();
+    </script>
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
 function show_add(){
-    Command: toastr["success"]("User added successfully", "Add User")
+  toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
 
-toastr.options = {
-  "closeButton": true,
-  "debug": false,
-  "newestOnTop": false,
-  "progressBar": false,
-  "positionClass": "toast-top-center",
-  "preventDuplicates": false,
-  "onclick": null,
-  "showDuration": "300",
-  "hideDuration": "1000",
-  "timeOut": "5000",
-  "extendedTimeOut": "1000",
-  "showEasing": "swing",
-  "hideEasing": "linear",
-  "showMethod": "fadeIn",
-  "hideMethod": "fadeOut"
+  toastr["info"]("User added successfully", "Add user")
+
 }
+
+function confirm_delete(){
+  
 }
